f5071995d0618cca24715420d622fc3d7c2a5c02
Alexey Kuznetsov <ak@axet.ru>
2010-11-17 02:54:47 +0300

    add utf-8 output

--- json2xml.py
+++ json2xml.py
@@ -44,7 +44,7 @@ def parse_json_example():
   js = r"""{"object": {"content": "bla bla bla", "type": "note", "links": {"alternate": [{"href": "http://www.google.com/buzz/111/222", "type": "text/html"}, {"href": "http://www.google.com/buzz/1111/2222", "type": "text/html"}]}}, "id": "tag:google.com,2010:buzz:1111", "links": {"alternate": [{"href": "http://www.google.com/buzz/11111/222", "type": "text/html"}]}}"""
   j = json.loads(js)
   doc = parse_doc("post", j)
-  print doc.toprettyxml(indent="  ")
+  print doc.toprettyxml(encoding="utf-8", indent="  ")
 
 def parse_json_stdin(root):
   js = "".join(sys.stdin.readlines())
